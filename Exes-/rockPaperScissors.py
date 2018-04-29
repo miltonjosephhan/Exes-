3edfb76a0d07ae551e3435e5d32981b110e21ea3
CDMS6\Han <miltonjosephhan@gmail.com>
2018-04-29 10:55:27 -0700

    Added range and input functions

--- Exes-/rockPaperScissors.py
+++ Exes-/rockPaperScissors.py
@@ -6,17 +6,6 @@ Created on Sun Apr  8 09:44:57 2018
 """
 
 
-#print('What is your name?')
-#name = input()
-
-#print('ok, ' + name + ' what is your age?')
-#age = int(input())
-
-#ducks = 100 - age 
-
-#print('You are ' + str(age) + ' years old.') 
-#print(' Interesting, you will be 100 in the year: ' + str(2018 + ducks))
-
 player1 = input('What is your name?')
 player2 = input('And what about you?')
 
@@ -26,6 +15,30 @@ p1_answer = input('What do you choose ' + player1 + '?')
 
 p2_answer = input('Okay, and what about you ' + player2 + '?')
 
+for i in range(3): 
+    p1_answer = input(str(player1) + ", please choose 'rock,' 'paper,' or 'scissors.' ")
+    if p1_answer == 'rock' or p1_answer == 'paper' or p1_answer == 'scissors': 
+        print('Valid Input')
+        break
+    if p1_answer != 'rock' or p1_answer != 'paper' or p1_answer != 'scissors':
+        print("You need to enter in 'rock,' 'paper,' or 'scissors.'")    
+      
+"""
+for p1_answer in range (3):
+    if p1_answer == 'rock' or 'paper' or 'scissors':
+    print("Guess again with the right entry") 
+       input(str(player1) + ", please choose 'rock,' 'paper,' or 'scissors' again. ")
+        break
+"""
+
+for i in range (3):
+    p2_answer = input(str(player2) + ", please choose 'rock,' 'paper,' or 'scissors.' ")
+    if p2_answer == 'rock' or p2_answer == 'paper' or p2_answer == 'scissors': 
+       print('Correct Input')
+       break
+    if p2_answer != 'rock' or p2_answer != 'paper' or p2_answer != 'scissors':     
+       print ("Please enter in 'rock,' 'paper,' or 'scissors.'")  
+       
 if p1_answer == p2_answer:
     print('Sorry its a tie, try again')
     
@@ -52,6 +65,13 @@ if p2_answer == 'paper':
 if p2_answer == 'scissors':
     if p1_answer == 'paper':
         print('Scissors beats paper ' + player2 + ' wins!')
+        
+        
+      
+
+   
+else: 
+       print("Invalid entry. Please choose 'rock,' 'paper,' or 'scissors' next time.")     
     
        
 
